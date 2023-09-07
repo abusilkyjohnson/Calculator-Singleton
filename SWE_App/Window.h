48528367c80b7c6af0f57b9485ded22552fba727
Abubakar Mohammed <ASMohammed@student.fullsail.edu>
2023-09-06 22:54:42 -0400

    all button made in .h

--- SWE_App/Window.h
+++ SWE_App/Window.h
@@ -3,8 +3,36 @@
 
 class Window : public wxFrame
 {
-	wxTextCtrl* textbox = nullptr;
+	wxTextCtrl* _textbox = nullptr;
 
+	wxButton* _zeroButt = nullptr;
+	wxButton* _oneButt = nullptr;
+	wxButton* _twoButt = nullptr;
+	wxButton* _threeButt = nullptr;
+	wxButton* _fourButt = nullptr;
+	wxButton* _fiveButt = nullptr;
+	wxButton* _sixButt = nullptr;
+	wxButton* _sevenButt = nullptr;
+	wxButton* _eightButt = nullptr;
+	wxButton* _nineButt = nullptr;
+
+	wxButton* _cosButt = nullptr;
+	wxButton* _sinButt = nullptr;
+	wxButton* _tanButt = nullptr;
+
+	wxButton* _modButt = nullptr;
+	wxButton* _multiplicationButt = nullptr;
+	wxButton* _divisionButt = nullptr;
+
+	wxButton* _additionButt = nullptr;
+	wxButton* _subtractionButt = nullptr;
+	wxButton* _negativeButt = nullptr;
+
+	wxButton* _equalsButt = nullptr;
+	wxButton* _backSpaceButt = nullptr;
+	wxButton* _clearButt = nullptr;
+
+	
 
 public:
 	Window();
