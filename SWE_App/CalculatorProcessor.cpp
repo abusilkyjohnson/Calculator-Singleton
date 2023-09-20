24691efdb94defd4860232e2f06733ebf206e061
Abubakar Mohammed <ASMohammed@student.fullsail.edu>
2023-09-20 00:10:26 -0400

    simple rough set up need to code calculations

--- SWE_App/CalculatorProcessor.cpp
+++ SWE_App/CalculatorProcessor.cpp
@@ -1,1 +1,14 @@
 #include "CalculatorProcessor.h"
+#include "Window.h"
+
+
+CalculatorProcessor* CalculatorProcessor::calcProcessorSingleton_ = nullptr;
+
+
+CalculatorProcessor* CalculatorProcessor::GetInstance()
+{
+	if (calcProcessorSingleton_ == nullptr)
+	{
+		calcProcessorSingleton_ = new CalculatorProcessor();
+	}
+}
--- SWE_App/CalculatorProcessor.h
+++ SWE_App/CalculatorProcessor.h
@@ -1,5 +1,22 @@
 #pragma once
-class CalculatorProcessor
+
+class Window;
+
+
+class CalculatorProcessor // my singleton
 {
+
+protected:
+
+	static CalculatorProcessor* calcProcessorSingleton_;
+	CalculatorProcessor(){};
+
+public:
+	CalculatorProcessor(CalculatorProcessor& other) = delete;
+	void operator=(const CalculatorProcessor&) = delete;
+
+
+	static CalculatorProcessor* GetInstance();
+	wxString CalculationAdd(wxString sign);
 };
 
